62db97411b8bffdf97bf983c5e8d854e26c99643
Gsalive <gsalive2002@gmail.com>
2023-06-19 13:37:27 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,100 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { compose } from 'ramda';
+import 'whatwg-fetch';
+import './style.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends Component {
+  state = {
+    text: '',
+   
+    wordCount: 0,
+ 
+  }
+
+  componentDidMount() {
+    this.getText()
+      .then(Text => {
+        this.setState({ text:('') }, () => this.setCounts(this.state.text));
+      })
+      .catch(err => this.setState({ text: `Error: ${err.message}` }));
+  }
+
+  /*
+    Fetches three parapgraphs from https://baconipsum.com/
+  */
+  getText = async () => {
+    
+    const body = ("");
+    return body;
+  }
+
+  /*
+    When pressing Enter key individual elements in the array will come
+    with the 'break' character and producing an incorrect word count. This function
+    removes those breaks and splits them.
+    i.e. ["↵↵Turkey","pork","cow","tri-tip","↵↵Bresaola↵↵brisket","pork"]
+  */
+  removeBreaks = arr => {
+    const index = arr.findIndex(el => el.match(/\r?\n|\r/g));
+    
+    if (index === -1) 
+      return arr;
+    
+    const newArray = [
+      ...arr.slice(0,index),
+      ...arr[index].split(/\r?\n|\r/),
+      ...arr.slice(index+1, arr.length)
+    ];
+
+    return this.removeBreaks(newArray);
+  }
+
+  /*
+    When entering multiple spaces or breaks (Enter key), the array will come
+    with empty elements. This function removes those elements. 
+    i.e. ["turkey.", "", "Bacon"]
+  */
+  removeEmptyElements = arr => {
+    const index = arr.findIndex(el => el.trim() === '');
+
+    if (index === -1) 
+      return arr;
+
+    arr.splice(index, 1);
+
+    return this.removeEmptyElements(arr)
+  };
+
+  setCounts = value => {
+    const trimmedValue = value.trim();
+    const words = compose(this.removeEmptyElements, this.removeBreaks)(trimmedValue.split(' '));
+   
+
+
+    this.setState({
+      text: value,
+      
+      wordCount: value === '' ? 0 : words.length,
+      
+    });
+  }
+
+  handleChange = e => this.setCounts(e.target.value);
+  
+  render() {
+    return (
+     
+      <div class="text-center mx-auto my-3 form-outline w-50 text-white" >
+        <style>{'body { background-color: #2C2C2D; }'}</style>
+        <h3>The Word Reader</h3><br/>
+        <textarea class="form-control text-white bg-dark" rows='15' onChange={this.handleChange} value={this.state.text}></textarea><br/>
+        <strong >Word Count:</strong>&nbsp; &nbsp;{this.state.wordCount}<br/>
+    </div>    
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
